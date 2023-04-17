8a6e4261c9ca741bd54f1d3853083ae28c5db2a3
Jeewoon Hong <jeewoon.hong@gmail.com>
2023-04-17 18:49:28 +0900

    typo fixed

--- google_translate.py
+++ google_translate.py
@@ -1,19 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
 import selenium
 import json
 import time
 
 
 DRIVER_PATH = "./chromedriver_mac_arm64/chromedriver"
-LANG_FROM = "en" # "auto" available
+LANG_FROM = "auto" # "en" available
 LANG_TO = "ko"
 JSON_PATH = [
     "./data/sg_90k_part1_html_cleaned.json",
     "./data/sg_90k_part2_html_cleaned.json"
 ]
 
-
+# 데이터 통계 (길이 분포, turn num, 언어 분포 ?, etc ..)
+# polyglot -> 처리할 때 통계 (얼마나 드랍되는지)
+# fastchat code 참고
 def preprocess_with_utterance_index(json_path):
     with open(json_path, "r") as original_json:
         json_list = json.load(original_json)
@@ -32,7 +35,7 @@ def preprocess_with_utterance_index(json_path):
             # concat_convs = concat_convs[:-3]
             concat_convs = []
             for i in range(0, len(original_convs)):
-                concat_convs += [original_convs[i]["from"] + str(i) + ":" + original_convs[i]["value"].replace("\n", "%0A")]
+                concat_convs += ["\"" + original_convs[i]["from"]  + str(i) + "\"" + ":" + original_convs[i]["value"].replace("\n", "%0A")]
             entry["conversations"] = concat_convs
             entry["turns"] = len(original_convs)
     return json_list
@@ -59,7 +62,7 @@ def set_proxy_server(proxy_retrieving_driver, trial=3, n=50):
             try:
                 driver.get("https://translate.google.co.in/")
                 return driver
-            except selenium.common.exceptions.WebDriverException:
+            except (WebDriverException, TimeoutException):
                 driver.quit()
     return None
     
@@ -125,7 +128,7 @@ def split_text_with_overlap_v2(dialogue, max_len, overlap):
     return splitted_dialogue, splitting_index
 
 
-def speaker_with_index(index):
+def speaker_with_utterance_index(index):
     if index%2 == 0:
         return "human" + str(index)
     else:
@@ -133,9 +136,9 @@ def speaker_with_index(index):
 
 
 def merge_splitted_translation(splitted_result, splitting_index):
-    merged = splitted_result[1]
+    merged = splitted_result[0]
     for i in range(0, len(splitted_result) - 1):
-        start_index = splitted_result[i+1].lower.find(speaker_with_index(splitting_index[i][1]))
+        start_index = splitted_result[i+1].lower.find(speaker_with_utterance_index(splitting_index[i][1]))
         merged += '\n' + splitted_result[i+1][start_index:]
     return merged
 
@@ -150,7 +153,7 @@ def translate(driver, lang_from, lang_to, dialogue, timeout=600, max_len=5000):
         else: # translated !
             return result
     else:
-        splitted_dialogue, splitting_index = split_text_with_overlap_v2(dialogue, max_len, overlap = 2500)
+        splitted_dialogue, splitting_index = split_text_with_overlap(dialogue, max_len, overlap = 2500)
         states_and_results = list(map(lambda x: get_tranlation_response_timeout(driver, lang_from, lang_to, x, timeout), splitted_dialogue))
         states, results = list(zip(*states_and_results))
         if -1 in state:
@@ -161,10 +164,10 @@ def translate(driver, lang_from, lang_to, dialogue, timeout=600, max_len=5000):
 
 def convert_translation_to_save_format(translation, turns):
     convs = []
-    start_index = translation.lower.find(speaker_with_index(0))
+    start_index = translation.lower.find(speaker_with_utterance_index(0))
     for i in range(0, turns-1):
-        speaker = speaker_with_index(i)
-        next_start_index = translation.lower.find(speaker_with_index(i+1))
+        speaker = speaker_with_utterance_index(i)
+        next_start_index = translation.lower.find(speaker_with_utterance_index(i+1))
         utterance = {}
         utterance["from"] = speaker[:-1]
         utterance["value"] = translation[start_index+len(speaker)+1:next_start_index-1] # exclued manually added newline character
